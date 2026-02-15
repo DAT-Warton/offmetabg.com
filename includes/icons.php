e9d6db3994582469a27f9fcff3f1656393b8d7c6
DAT Warton <datwarton@gmail.com>
2026-02-15 09:17:29 +0200

    Add missing icon_heart and icon_log_out functions to fix 500 error

--- includes/icons.php
+++ includes/icons.php
@@ -316,3 +316,17 @@ function icon_arrow_right($size = 20, $color = 'currentColor') {
         <polyline points="12 5 19 12 12 19"></polyline>
     </svg>';
 }
+
+function icon_heart($size = 20, $color = 'currentColor') {
+    return '<svg width="' . $size . '" height="' . $size . '" viewBox="0 0 24 24" fill="none" stroke="' . $color . '" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: inline-block; vertical-align: middle;">
+        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+    </svg>';
+}
+
+function icon_log_out($size = 20, $color = 'currentColor') {
+    return '<svg width="' . $size . '" height="' . $size . '" viewBox="0 0 24 24" fill="none" stroke="' . $color . '" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: inline-block; vertical-align: middle;">
+        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+        <polyline points="16 17 21 12 16 7"></polyline>
+        <line x1="21" y1="12" x2="9" y2="12"></line>
+    </svg>';
+}
